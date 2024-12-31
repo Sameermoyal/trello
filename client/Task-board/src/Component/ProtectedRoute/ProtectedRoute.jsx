91b7af3b827abbf9d252d42a4b70fda41db6129d
Sameermoyal <90440735+Sameermoyal@users.noreply.github.com>
2024-12-31 14:40:43 +0530

    Delete client directory

--- client/Task-board/src/Component/ProtectedRoute/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-
-function ProtectedRoute({}) {
-  return isAuthenticated ? <Navigate to='/'/> :<Navigate to='/login'/>
-}
-
-export default ProtectedRoute
